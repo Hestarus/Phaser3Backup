a1ee9595e49a87b59a2cb8ac8041f39ba7d5c779
Nathanael Zamora <deafendttack39@gmail.com>
2022-01-24 13:47:36 +0700

    Update scnPlayCreate.js

--- scnPlayCreate.js
+++ scnPlayCreate.js
@@ -12,8 +12,8 @@ function create(){
         var BG = this.add.image(bg_x, 768/2, 'fg_loop_back');
         var FG = this.add.image(bg_x, 768/2, 'fg_loop');
         
-        BG.setData('kecepatan', 2);
-        FG.setData('kecepatan', 4);
+        BG.setData('kecepatan', 4); //2
+        FG.setData('kecepatan', 8); //4
         FG.setDepth(2);
         
         bg_awal.push(BG);
@@ -28,6 +28,25 @@ function create(){
     this.chara.setDepth(3);
     this.chara.setScale(0);
     
+    //MENAMBAHKAN PARTIKEL
+    this.trail = this.add.particles('ptc_blue');
+    this.trailEmitter = this.trail.createEmitter({
+        x:0,
+        y:0,
+        angle: {min: 0, max: 0,},
+        scale: {start: 1, end: 0},
+        blendmode: 'SCREEN',
+        lifespan: 400,
+        speed:100,
+        on: true,
+        follow: this.chara,
+        tint: 0xff1d00
+    })
+    
+    this.trailEmitter.emitParticle(16);
+    this.trail.setDepth(2);
+    this.trail.setVisible(false);
+    
     // this.isGameRunning = false;
 	
 	var myScene = this;
@@ -41,8 +60,21 @@ function create(){
 	    scaleY: 1,
 	    onComplete: function(){
 	        myScene.isGameRunning = true;
+	        myScene.trail.setVisible(true)
 	    }
 	});
+	
+	//SUARA KETIKA BERTABRAKAN
+	this.snd_dead = this.sound.add('snd_dead');
+	
+	this.snd_click = [];
+	this.snd_click.push(this.sound.add('snd_klik_1'));
+	this.snd_click.push(this.sound.add('snd_klik_2'));
+	this.snd_click.push(this.sound.add('snd_klik_3'));
+	
+	for(let i = 0; i < this.snd_click.length; i++){
+	    this.snd_click[i].setVolume(0.5);
+	}
 	
 	//Menambahkan panel score
 	this.score = 0;
@@ -80,6 +112,8 @@ function create(){
             duration: 750,
             y: this.chara.y + 200
         })
+        
+        this.snd_click[Math.floor((Math.random()*2))].play();
     }, this)
 	
 }
