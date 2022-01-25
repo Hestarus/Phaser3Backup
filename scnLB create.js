f6a0a201fa47dc181619eeff42c02d69b6395360
Nathanael Zamora <deafendttack39@gmail.com>
2022-01-25 17:10:14 +0700

    Update scnLB create.js

--- scnLB create.js
+++ scnLB create.js
@@ -23,9 +23,20 @@ function create(){
     this.add.image(X_POSITION.CENTER, Y_POSITION.CENTER, 'bg_start');
     var panel = this.add.image(X_POSITION.CENTER, Y_POSITION.CENTER, 'panel_skor');
     panel.setOrigin(0.5);
+    panel.setDepth(10);
+    panel.setAlpha(0.8);
+    
+    this.score = localStorage["highscore_LB"];
+    var label_score = this.add.text(panel.x + 25, panel.y, "Score Terakhir: "+ this.score);
+    label_score.setOrigin(0.5);
+    label_score.setDepth(10);
+    label_score.setFontSize(30);
+    label_score.setTint(0xff732e);
+    
     var btnBack = this.add.image(panel.x + 210, Y_POSITION.CENTER, 'btn_play');
     btnBack.setScale(0.5);
     btnBack.setInteractive();
+    
     
     this.input.on('gameobjectup', function (pointer, gameObject) {
         // console.log("Scene Menu | Object End Click");
