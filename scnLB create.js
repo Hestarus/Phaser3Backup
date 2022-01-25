08c6682cf3a98c3ea320e809f5d122ee6f447153
Nathanael Zamora <deafendttack39@gmail.com>
2022-01-25 16:58:21 +0700

    Update scnLB create.js

--- scnLB create.js
+++ scnLB create.js
@@ -21,4 +21,20 @@ function create(){
     }
     
     this.add.image(X_POSITION.CENTER, Y_POSITION.CENTER, 'bg_start');
+    var panel = this.add.image(X_POSITION.CENTER, Y_POSITION.CENTER, 'panel_skor');
+    panel.setOrigin(0.5);
+    var btnBack = this.add.image(panel.x + 210, Y_POSITION.CENTER, 'btn_play');
+    btnBack.setScale(0.5);
+    btnBack.setInteractive();
+    
+    this.input.on('gameobjectup', function (pointer, gameObject) {
+        // console.log("Scene Menu | Object End Click");
+
+        if (gameObject == btnBack) {
+            btnBack.setTint(0xffffff);
+            this.scene.start('scnMenu');
+            this.snd_touch.play();
+        }
+    }, this);
+    
 }
