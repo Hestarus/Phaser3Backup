51ede90ddf501e3be7b00e3e39a35873fa695f58
Nathanael Zamora <deafendttack39@gmail.com>
2022-01-25 16:48:52 +0700

    Update scnLB create.js

--- scnLB create.js
+++ scnLB create.js
@@ -1,5 +1,4 @@
-function create() {
-
+function create(){
     X_POSITION =
     {
         'LEFT': 0,
@@ -13,17 +12,13 @@ function create() {
         'CENTER': game.canvas.height / 2,
         'BOOTOM': game.canvas.height,
     };
-    
-    var highscore = localStorage["highscore"] || 0; 
 
-    this.add.image(X_POSITION.CENTER, Y_POSITION.CENTER, 'bg_start')
+    if (snd_ambience == null) {
+        snd_ambience = this.sound.add('snd_ambience');
+        snd_ambience.loop = true;
+        snd_ambience.setVolume(0.35);
+        snd_ambience.play();
+    }
     
-    //=====PANEL LEADERBOARD=====
-    var panel = this.add.image(X_POSITION.CENTER, Y_POSITION.CENTER, 'panel_skor');
-    
-    var scoreText = this.add.text(panel.x + 25, panel.y, "Highscore: "+highscore)   
-    scoreText.setOrigin(0.5);
-    scoreText.setDepth(10);
-    scoreText.setFontSize(30);
-    scoreText.setTint(0xff732e);``
+    this.add.image(X_POSITION.CENTER, Y_POSITION.CENTER, 'bg_start');
 }
