2e495eeece7e5af44f654a5df7e06e3424a0360c
Nathanael Zamora <deafendttack39@gmail.com>
2022-01-26 08:57:18 +0700

    Update scnLB create.js

--- scnLB create.js
+++ scnLB create.js
@@ -24,14 +24,24 @@ function create(){
     var panel = this.add.image(X_POSITION.CENTER, Y_POSITION.CENTER, 'panel');
     panel.setOrigin(0.5);
     panel.setDepth(10);
-    panel.setAlpha(0.8);
+    panel.setAlpha(1);
     
-    this.score = localStorage["highscore_LB"];
-    var label_score = this.add.text(panel.x - 85, panel.y -170, "Score Terakhir: "+ this.score);
+    var highscoreLB = localStorage["highscore_LB"];
+    var label_score = this.add.text(panel.x - 85, panel.y -170, "Score Terakhir: "+ highscoreLB);
     label_score.setOrigin(0.5);
     label_score.setDepth(10);
     label_score.setFontSize(30);
     label_score.setTint(0xff732e);
+    
+   
+    
+    
+    
+    
+    
+    
+    
+    
     
     var btnBack = this.add.image(panel.x + 210, Y_POSITION.CENTER, 'btn_play');
     btnBack.setScale(0.5);
