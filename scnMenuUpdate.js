83edda355c03204455b3aa7452ba0f7560306a04
Nathanael Zamora <deafendttack39@gmail.com>
2022-02-18 10:36:44 +0700

    Update scnMenuUpdate.js

--- scnMenuUpdate.js
+++ scnMenuUpdate.js
@@ -1,15 +1,15 @@
 function update(){
-    for (let i = 0; i < this.backgrounds.length; i++)
-	{
-	    for (var j = 0; j < this.backgrounds[i].length;  j++)
-	    {
-	        this.backgrounds[i][j].x -= this.backgrounds[i][j].getData('kecepatan');
-	        if  (this.backgrounds[i][j].x <= -(1366/2)){
-	            var diff = 1366/2 * this.backgrounds[i].length - 3;
-	            this.backgrounds[i][j].x = 1366 + 1366/2 + diff;
-	        }
-	    }
-	}
+
+	
+	for (let i = 0; i < this.backgrounds.length; i++) {
+            for (var j = 0; j < this.backgrounds[i].length; j++) {
+                this.backgrounds[i][j].x -= this.backgrounds[i][j].getData('kecepatan');
+                if (this.backgrounds[i][j].x <= -(1366 / 2)) {
+                    var diff = 1366 / 2 * this.backgrounds[i].length-3;
+                    this.backgrounds[i][j].x = 1366 + 1366 / 2 + diff;
+                }
+            }
+        }
 	
 	if (this.timerHalangan == 0) {
         var acak_y = Math.floor((Math.random() * 680) + 60);
@@ -34,4 +34,11 @@ function update(){
     for (var i = this.halangan.length - 1; i >= 0; i--) {
     
     }
+        var enter = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE);
+        if(enter.isDown){
+            // btnPlay.setTint(0xffffff);
+            this.scene.start('scnPlay');
+            this.snd_touch.play();
+            console.log("Enter")
+        }
 }
